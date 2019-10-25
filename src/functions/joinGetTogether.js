94530506a0e0270e74ae0492eeaf8231b131b731
Jordi <jordism91@gmail.com>
2019-10-25 09:53:04 +0200

    deploy

--- src/functions/joinGetTogether.js
+++ src/functions/joinGetTogether.js
@@ -1,5 +1,25 @@
-module.exports.handler = async (event) => {
-    console.log(event);
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+const chance = require('chance').Chance();
 
-    return 'all done';
+module.exports.handler = async (event) => {
+  const parsedBody = JSON.parse(event.body);
+  const orderId = chance.guid();
+
+  const params = {
+    Message: JSON.stringify({
+      getTogetherId: parsedBody.getTogetherId,
+      userEmail: parsedBody.userEmail,
+      orderId
+    }),
+    TopicArn: process.env.joinGetTogetherSnsTopic
+  };
+  
+  const response = await sns.publish(params).promise();
+  console.log(response);
+
+  return {
+      statusCode: 200,
+      body: JSON.stringify({orderId})
+  }
 };
